63f10c596a0b57ab5933b020936b7f8215cb7dfb
Brandon Fergerson <bfergerson@apache.org>
2021-01-23 12:27:44 -0500

    spelling

--- src/main/groovy/com/codebrig/phenomena/ConnectionException.groovy
+++ src/main/groovy/com/codebrig/phenomena/ConnectionException.groovy
@@ -1,7 +1,7 @@
 package com.codebrig.phenomena
 
 /**
- * Thrown when the connection to a service (Grakn/Bbblsh) is or becomes unavailable.
+ * Thrown when the connection to a service (Grakn/Babelfish) is or becomes unavailable.
  *
  * @since 0.3.0
  * @author <a href="mailto:brandon.fergerson@codebrig.com">Brandon Fergerson</a>
